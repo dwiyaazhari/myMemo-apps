91b0c54122fc87ae47909e416c79ff50eaae3d39
dwiya azhari <dwiyaazhari56@gmail.com>
2021-10-31 19:02:40 +0700

    myMemo-apps

---
+++ app/src/main/java/com/example/mymemo/Android_4.java
@@ -0,0 +1,14 @@
+package com.example.mymemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Android_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.android_4);
+    }
+}
